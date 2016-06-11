090f8879f2932a45e761fcc34be2e1cbce35bcac
Alevardi <amet.alvirde@gmail.com>
2016-06-10 22:10:35 -0500

    :metal: tmux and emacs init conf's added :rocket: install script rewritten in json

---
+++ init.el
@@ -0,0 +1,13 @@
+;; Emacs 24 init.el
+;; Based on http://github.com/eschulte/emacs24-starter-kit
+
+;; NOTE: The name of the Org files is important!  When a file gets tangled,
+;; it gets the same base name as the Org file.  Thus, tangling Emacs Lisp from
+;; a file `init.org` would generate `init.el`, obliterating this file in the
+;; process. So your config org file should not be named "init.org".
+
+(require 'ob-tangle)
+;; (setq debug-on-error t)
+(org-babel-load-file
+ (expand-file-name "emacs-init.org"
+                   user-emacs-directory))
