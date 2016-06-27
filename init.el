b89cc6dc6e9289f5f5e6c1c0d73f88df005b797b
Alevardi <amet.alvirde@gmail.com>
2016-06-26 19:30:42 -0500

    :rocket: Linum-mode and git-gutter added. Basically configured. To be refined

--- init.el
+++ init.el
@@ -17,6 +17,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(wakatime-api-key "getenv(\"WAKA_TIME_API_KEY\")"))
+ '(git-gutter:update-interval 2)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
